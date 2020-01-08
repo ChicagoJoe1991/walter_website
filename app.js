85abd1a46305986a51a9c060923a256051a5d327
Walter <eschenbach.walter@gmail.com>
2020-01-07 23:51:06 -0500

    mobile nav update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-bar');
+    const nav = document.querySelector('.nav-ul');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
