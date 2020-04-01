ec6c6ef712bce5e3a66cb7a6c20583025e3554d8
Walter <eschenbach.walter@gmail.com>
2020-04-01 10:16:37 -0500

    updated project list and canvas

--- app.js
+++ app.js
@@ -1,78 +1,76 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-list');
-    const navLinks = document.querySelectorAll('.nav-list li')
-    
-    
-    
-    burger.addEventListener('click', () => {
-        //toggle nav
-        nav.classList.toggle('nav-active');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-list");
+  const navLinks = document.querySelectorAll(".nav-list li");
 
-        //animate links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = '';
-            }else{
-            link.style.animation = `navLinkFade 2s ease forwards ${index / 5}s`;
-            }
-        })
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
 
-        //burger animation
-        burger.classList.toggle('bun');
-        
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 2s ease forwards ${index / 5}s`;
+      }
     });
 
-    
-}
+    //burger animation
+    burger.classList.toggle("bun");
+  });
+};
 navSlide();
 // // // pin nav bar to top of mobile view port
 
 // // //when the user scrolls the page, execute stickyNav() function
 
- window.onscroll = function() {stickyNav()};
+window.onscroll = function() {
+  stickyNav();
+};
 // // //get the nav bar
- let navbar = document.getElementById('nav-bar');
+let navbar = document.getElementById("nav-bar");
 // // //get the offset position of the navbar
- let snavbar = navbar.offsetTop;
- console.log(snavbar);
+let snavbar = navbar.offsetTop;
+console.log(snavbar);
 // // //add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNav(){
-     if(window.pageYOffset >= snavbar){
-       navbar.classList.add('snavbar')
-   }else{
-         navbar.classList.remove('snavbar');
-    }
- }
-
+function stickyNav() {
+  if (window.pageYOffset >= snavbar) {
+    navbar.classList.add("snavbar");
+  } else {
+    navbar.classList.remove("snavbar");
+  }
+}
 
 //project notecard animations
 
 $(document).ready(function() {
-    $('.proj-list a li').hover(function() {
-        $(this).animate({
-            left: 20,
-            bottom: 20,
-            height: '310px',
-            width: '520px',
-            opacity: '0.95', 
-        });
+  $(".proj-list a li").hover(
+    function() {
+      $(this).animate({
+        left: 20,
+        bottom: 20,
+        height: "310px",
+        width: "475px",
+        opacity: "0.95"
+      });
     },
     function() {
-        $(this).animate({
-            left: 0,
-            bottom: 0,
-            height: '300px',
-            width: '500px',
-            opacity: '1' 
-        });
-    });
+      $(this).animate({
+        left: 0,
+        bottom: 0,
+        height: "300px",
+        width: "450px",
+        opacity: "1"
+      });
+    }
+  );
 });
 
-// scroll changing opacity 
+// scroll changing opacity
 const navOpacity = () => {
-    let nav = document.getElementById('proj-ul');
-    console.log(nav);
-    nav.style.opacity = 1;
-}
+  let nav = document.getElementById("proj-ul");
+  console.log(nav);
+  nav.style.opacity = 1;
+};
 navOpacity();
